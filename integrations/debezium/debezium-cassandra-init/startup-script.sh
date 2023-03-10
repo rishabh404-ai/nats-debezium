fb1f5eced42bc1b2ec7b5e14f732f611db148289
Rishabh Mishra <rmishra.itp@sparkscognition.com>
2023-03-10 16:28:44 +0530

    updated code

--- integrations/debezium/debezium-cassandra-init/startup-script.sh
+++ integrations/debezium/debezium-cassandra-init/startup-script.sh
@@ -4,10 +4,11 @@ sh /opt/cassandra/bin/cassandra -f &
 
 while ! grep -q "Created default superuser role 'cassandra" /opt/cassandra/logs/system.log
 do
-  sleep 1
+  sleep 120
 done;
-
+printf 'Create and populate the database.\n'
 cqlsh -f $DEBEZIUM_HOME/inventory.cql
+printf 'Database Execution finished\n'
 
 while true;
 do
